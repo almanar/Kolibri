a77c2c2b47fde698ee053fd6b21e8d895aa78ce9
almanar <menar@live.se>
2016-10-26 00:35:30 +0200

    A http request fixed and added functionality for missed_packets/auto_reset. update

--- source/config.h
+++ source/config.h
@@ -1,16 +1,16 @@
 /*					Configurable macros						*/
 /*----------------------------------------------------------*/
 /*					SIM INTERNET SETTINGS					*/
-#define CM_CUST_APN    				"4g.tele2.se"         	// The APN of your SIM
-#define CM_USING_PROXY 				VM_FALSE              	// Whether your SIM uses proxy
+#define CM_CUST_APN    				"your.apn.set"         	// The APN of your SIM
+#define CM_USING_PROXY 				0		              	// Whether your SIM uses proxy
 #define CM_PROXY_ADDRESS   			"0.0.0.0"	          	// The proxy address
 #define CM_PROXY_PORT  				80                    	// The proxy port
 
 /*					SYNC SETTINGS							*/
+#define CM_MY_CONTROL_NUMBER    	   	"12345"
+#define CM_MY_WEBSERVICE_URL	        "your_app.appspot.com/receiver.cgi"
 #define CM_MY_WEBSERVICE_REPLY          "!ACK"
 #define CM_MY_USER_AGENT 				"xDrip"
-#define CM_MY_CONTROL_NUMBER    	   	"10121"
-#define CM_MY_WEBSERVICE_URL	        "http://noorparakeet.appspot.com/receiver.cgi"
 
 /*					CGM SETTINGS							*/
-#define CM_TRANSMITTER_ID  				"6EUEQ"             //
+#define CM_TRANSMITTER_ID  				"ABCDE"             //
--- source/dexcomg4.c
+++ source/dexcomg4.c
@@ -16,6 +16,7 @@
 #include "config.h"
 
 dexcom_g4_packet dexg4_packet={0};
+VMUINT32 RX_POLL_TIME =0;
 
 static VMUINT8 ASCII_SRC_NAME[6];
 static VMUINT8  CHAN_HOPP[4]     = {0,100,199,209};
@@ -23,8 +24,6 @@ static VMINT8   CHAN_OFFSET[4]   = {0,0,0,0};//{0xE4, 0xE3, 0xE2, 0xE2};
 static VMUINT32 RX_CH_TIMEOUT[4] = { 500 , 1000 , 1500 , 2100 };
 static VMUINT8 RX_BUFFER[4][21]  = {{0}};
 
-VMUINT32 RX_POLL_TIME =0;
-VMUINT RX_POLL_TIME_EPOCH=0;
 
 VMUINT8  SRC_NAME_TABLE[32] = { '0', '1', '2', '3', '4', '5', '6', '7',
 								'8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
@@ -36,6 +35,7 @@ VMUINT8  SRC_NAME_TABLE[32] = { '0', '1', '2', '3', '4', '5', '6', '7',
 void delay(int x){vm_thread_sleep(x);};
 VMUINT32 microsec(){return vm_time_ust_get_count();};
 VMUINT32 milisec(){delay(1); return vm_time_ust_get_count()/1000;};
+
 
 VMUINT8 get_packet_passed_checksum (VMUINT8 lqi)
 {
@@ -123,7 +123,7 @@ void dexcom_src_to_ascii (VMUINT32 psrc_addr)
 	ASCII_SRC_NAME[5] = 0;
 }
 
-void swap_channel(VMUINT8 index)
+void dex4_change_channel(VMUINT8 index)
 {
 	cc2500_send_strobe(CC2500_CMD_SIDLE);
 	cc2500_write_reg(REG_CHANNR, CHAN_HOPP[index]);
@@ -131,16 +131,26 @@ void swap_channel(VMUINT8 index)
 
 	cc2500_send_strobe(CC2500_CMD_SFRX);
 	cc2500_send_strobe(CC2500_CMD_SRX);
+
 	while ((cc2500_read_status_reg(REG_MARCSTATE) & 0x1F) != 0x0D) {};
 }
 
-void dexg4_stop(){
-	cc2500_send_strobe(CC2500_CMD_SPWD);
+VMUINT dexg4_init(){
+	cc2500_init2();
+	cc2500_read_config_regs();
 }
 
-VMUINT8 *dexg4_recieve(){
+VMUINT8 dexg4_wake(){
+	return cc2500_send_strobe(CC2500_CMD_SNOP);
+}
+
+VMUINT8 dexg4_sleep(){
+	return cc2500_send_strobe(CC2500_CMD_SPWD);
+}
+
+VMUINT8 dexg4_receive(){
 	vm_log_info("dexg4_recieve");
-	VMUINT   rx_polling = 1;
+	VMUINT8  rx_packet_status = 0;
 	VMUINT8  channel = 0;
 	VMUINT32 post_ch_timmer[4] = {0};
 
@@ -148,7 +158,7 @@ VMUINT8 *dexg4_recieve(){
 
 	do {
 		vm_log_info("channel: %d", channel);
-		swap_channel(channel);
+		dex4_change_channel(channel);
 
 		while( get_miso() == 0 ){
 			if (RX_POLL_TIME == 0){ delay(1); continue; }
@@ -159,7 +169,7 @@ VMUINT8 *dexg4_recieve(){
 		post_ch_timmer[channel] = milisec();
 
 		delay(50);
-		vm_log_info("slen: %d", cc2500_read_status_reg(REG_PKTLEN));
+		//vm_log_info("slen: %d", cc2500_read_status_reg(CC2500_REG_RXBYTES));
 		cc2500_read_burst_reg(CC2500_REG_RXFIFO, &dexg4_packet, 0x15);
 		vm_log_info("len: %d", dexg4_packet.pkt_len);
 		if ( dexg4_packet.pkt_len == 0x12 ){
@@ -167,36 +177,76 @@ VMUINT8 *dexg4_recieve(){
 				dexcom_src_to_ascii(dexg4_packet.src_addr);
 				if ( strcmp( ASCII_SRC_NAME, CM_TRANSMITTER_ID) == 0 ){
 					if (RX_POLL_TIME == 0 && channel == 0){
-						RX_POLL_TIME = post_ch_timmer[0] - 475;
-						vm_time_get_unix_time(&RX_POLL_TIME_EPOCH);
-						RX_POLL_TIME_EPOCH --;
+						// save timer time - 450 ms
+						RX_POLL_TIME = post_ch_timmer[0] - 450;
 					}
 
+					// save offsets
 					CHAN_OFFSET[channel] += cc2500_read_status_reg(0x32);
-					rx_polling = 0;
+					rx_packet_status = 1;
+
+				// Filter out unwanted behavior
 				}else{
 					vm_log_info("NOT MY DEX ID: %s", ASCII_SRC_NAME);
+					memset(&dexg4_packet, 0, sizeof(dexg4_packet));
+					continue;
 				}
 			}else{ memset(&dexg4_packet, 0, sizeof(dexg4_packet)); }
 		}else{ memset(&dexg4_packet, 0, sizeof(dexg4_packet)); }
 
+		// Stay on channel 0 first time
 		if(RX_POLL_TIME != 0){ channel++; }
+		// Exit when all channels are swept
+		if(channel==4){break;}
 
-	} while (rx_polling);
+	} while (!rx_packet_status);
 
 	RX_POLL_TIME = RX_POLL_TIME + 300000;
-	RX_POLL_TIME_EPOCH = RX_POLL_TIME_EPOCH -285;
 
 	vm_log_info("DEX ID: %s", ASCII_SRC_NAME);
 	vm_log_info("Dex id %08x", dexg4_packet.src_addr);
 
-	// On error or missed packets exit NULL
-	//return NULL;
+	return rx_packet_status;
 }
 
-VMUINT dexg4_init(){
-	cc2500_init2();
-	//cc2500_init3();
-	cc2500_read_config_regs();
-}
 
+/*-------------------------------Dummy----------------------------------*/
+VMUINT8 DUMMY_RX_BUFFER[21] = { 0x12,
+								0xFF,0xFF,0xFF,0xFF,
+								0x4D,0x6E,0x64,0x00,
+								0x3F,
+								0x03,
+								0x93,
+								0x93,0xCD,
+								0x1D,0xC9,
+								0xD2,
+								0x00,
+								0xAD,
+								0xB0,
+								0x40};
+
+VMBOOL dexg4_receive_dummy(){
+	int channel=0;
+
+	if (RX_POLL_TIME == 0) { delay(5000);}
+	memcpy(&dexg4_packet, &DUMMY_RX_BUFFER, sizeof(DUMMY_RX_BUFFER));
+
+	RX_POLL_TIME = milisec() - 475;
+
+	if ( dexg4_packet.pkt_len == 0x12 ){
+		if (get_packet_passed_checksum(dexg4_packet.lqi)){
+			dexcom_src_to_ascii(dexg4_packet.src_addr);
+			if ( strcmp( ASCII_SRC_NAME, CM_TRANSMITTER_ID) == 0 ){
+				if (RX_POLL_TIME == 0 && channel == 0){
+					RX_POLL_TIME = milisec() - 475;
+				}
+			}
+		}
+	}
+
+	RX_POLL_TIME = RX_POLL_TIME + 60000;
+
+	vm_log_info("DEX ID: %s", ASCII_SRC_NAME);
+	vm_log_info("Dex id %08x", dexg4_packet.src_addr);
+}
+/*-------------------------------Dummy----------------------------------*/
